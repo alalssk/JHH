3e8378a7e59f97d2a9969c0e34249b8f1c102e61
jhh <alalssk@gmail.com>
2019-07-30 16:36:11 +0900

    수정

---
+++ JHHCorelib/TCPCore/AsyncUserToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPCore
+{
+    public class AsyncUserToken
+    {
+        public System.Net.Sockets.Socket Socket { get; set; }
+    }
+}
